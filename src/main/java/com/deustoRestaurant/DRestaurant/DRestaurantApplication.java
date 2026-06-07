3f0237003e94a407253505b4865db9cc46f3a56b
Iker Larrinaga <iker.l.f@opendeusto.es>
2026-06-07 19:55:19 +0200

    Initial commit

---
+++ src/main/java/com/deustoRestaurant/DRestaurant/DRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.deustoRestaurant.DRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deustoRestaurant/DRestaurant/DRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deustoRestaurant.DRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
