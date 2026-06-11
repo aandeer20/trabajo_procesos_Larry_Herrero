5b0bf89144825abd7da8fc860ebdd4df544d6016
Iker Larrinaga <iker.l.f@opendeusto.es>
2026-06-11 11:43:53 +0200

    Corregir configuración de tests: H2 con restricciones FK desactivadas

--- src/test/java/com/deustoRestaurant/DRestaurant/DRestaurantApplicationTests.java
+++ src/test/java/com/deustoRestaurant/DRestaurant/DRestaurantApplicationTests.java
@@ -2,8 +2,10 @@ package com.deustoRestaurant.DRestaurant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DRestaurantApplicationTests {
 
 	@Test
